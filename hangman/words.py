d8b312762b5661260ee0dd1907e3880dcca61e86
Nikola Redzic <nrjob92@gmail.com>
2023-08-15 22:12:32 +0200

    hangman

---
+++ hangman/words.py
@@ -0,0 +1,13 @@
+word_list = [
+    "aardvark", "baboon", "camel", "dog", "elephant",
+    "fox", "giraffe", "horse", "iguana", "jaguar",
+    "kangaroo", "lemur", "mongoose", "newt", "otter",
+    "penguin", "quokka", "raccoon", "squirrel", "tiger",
+    "uakari", "vulture", "walrus", "xenops", "yak",
+    "zebra", "alpaca", "bear", "cat", "dolphin",
+    "eagle", "flamingo", "goat", "hamster", "ibex",
+    "jellyfish", "koala", "lion", "monkey", "narwhal",
+    "octopus", "panda", "quail", "rhinoceros", "seagull",
+    "toucan", "unicorn", "viper", "weasel", "jet",
+    "yabby", "zebu"
+]
